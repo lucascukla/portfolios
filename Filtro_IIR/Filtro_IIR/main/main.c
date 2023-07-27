5e843d623476c1242e8f911433ba35033525cdb2
lucascukla <lucascukla1994@gmail.com>
2023-07-27 08:05:59 -0300

    sigue con mal desempeño el filtro, debo revisar si no es la logica en assembler

--- Filtro_IIR/Filtro_IIR/main/main.c
+++ Filtro_IIR/Filtro_IIR/main/main.c
@@ -2,10 +2,10 @@
 #include <math.h>
 
 #include "driver/gpio.h"
-// #include "freertos/FreeRTOS.h"
-// #include "freertos/projdefs.h"
-// #include "freertos/task.h"
-// #include "freertos/queue.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_log.h"
 #include "esp_rom_sys.h"
 #include "sdkconfig.h"
@@ -14,8 +14,8 @@
 
 // constantes
 #define MPI 3.14159265358979323846  // numero pi
-#define Fs  48000.0                 // frecuencia de muestreo de la señal a fitrar
-#define N_periodos_test 5           // cantidad de periodos para el test unitario
+#define Fs  1000.0                 // frecuencia de muestreo de la señal a fitrar
+#define N_periodos_test 10           // cantidad de periodos para el test unitario
 
 
 struct filtro_IIR_2ord  // estructura que contiene los coeficientes deun filtro IIR de una etapa
@@ -41,27 +41,27 @@ extern void producto_y_acumulacion(float *_y, float* out, float *_acu, float *_k
 // defino los parametros de mi filtro notch de frecuencia central a 50 Hz
 // el filtro se diseñó utilizando la herramienta "Filter Builder" de Matlab(R)
 // la freuencia de muestreo de la señal es 48 kHz
-const struct filtro_IIR_2ord etapa_0 = {.b_0 = 1.000021338462830, .b_1 = -2, .b_2 = 1.000021338462830, .a_1 = -1.999893784523010, .a_2 = 0.999935925006867, .k_f = 0.0000005791732746729394};
-const struct filtro_IIR_2ord etapa_1 = {.b_0 = 1.000021457672119, .b_1 = -2, .b_2 = 1.000021457672119, .a_1 = -1.999891281127930, .a_2 = 0.999934792518616, .k_f = 1};
-const struct filtro_IIR_2ord etapa_2 = {.b_0 = 1.000021457672119, .b_1 = -2, .b_2 = 1.000021457672119, .a_1 = -1.999827861785889, .a_2 = 0.999870777130127, .k_f = 1};
-const float k_salida = 1.726264875*1000000; // constante de salida del filtro (cuarto valor de escala de la matriz SOS)
+const struct filtro_IIR_2ord etapa_0 = {.b_0 = 0.3813687, .b_1 = -0.7254062, .b_2 =0.3813683, .a_1 = -1.9018863, .a_2 = 0.9987550, .k_f = 1};
+const struct filtro_IIR_2ord etapa_1 = {.b_0 = 1.0514621, .b_1 = -2, .b_2 = 1.0514621, .a_1 = -1.8999511, .a_2 = 0.9987431, .k_f = 1};
+const struct filtro_IIR_2ord etapa_2 = {.b_0 = 1.0514631, .b_1 = -2, .b_2 = 1.0514613, .a_1 = -1.8978416, .a_2 = 0.9955087, .k_f = 1};
+const float k_salida = 2.363452434539795; // constante de salida del filtro (cuarto valor de escala de la matriz SOS)
 
 void app_main(void)
 {
     // printf("hola\n");
     test_unitario_filtro();
     while(1){
-        printf("no hago nada en el loop");
-        esp_rom_delay_us(1000000);
+        printf("no hago nada en el loop\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
 void test_unitario_filtro(void){
     printf("\n\ntest unitario filtro\n");
-    float frecuencias[23] = {47.000000, 48.000000, 49.000000, 49.200000, 49.400000, 49.600000, 49.700000, 49.800000, 49.850000, 49.900000, 49.950000, 50.000000, 50.050000, 50.100000, 50.150000, 50.200000, 50.300000, 50.400000, 50.600000, 50.800000, 51.000000, 52.000000, 53.000000};
-    float dB_ideal[23] = {-0.139634, -0.223699, -0.567321, -0.885751, -2.049284, -8.007648, -15.732472, -28.792566, -39.621588, -67.563333, -56.261444, -63.416721, -104.629457, -52.250306, -36.895746, -27.205495, -14.655357, -7.224630, -1.573171, -0.424280, -0.120204, 0.074168, 0.070265};
+    float frecuencias[] = {10.000000, 20.000000, 30.000000, 35.000000, 40.000000, 45.000000, 47.000000, 48.000000, 49.000000, 49.100000, 49.200000, 49.300000, 49.400000, 49.500000, 49.600000, 49.700000, 49.800000, 49.850000, 49.900000, 49.950000, 49.975000, 50.000000, 50.025000, 50.050000, 50.100000, 50.150000, 50.200000, 50.300000, 50.400000, 50.500000, 50.600000, 50.700000, 50.800000, 50.900000, 51.000000, 52.000000, 53.000000, 55.000000, 60.000000, 70.000000, 80.000000, 90.000000, 100.000000, 150.000000, 250.000000};//, 400.000000, 600.000000, 1000.000000};
+    float dB_ideal[] = {-0.000001, -0.000017, -0.000074, -0.000162, -0.000428, -0.001940, -0.005520, -0.012123, -0.037803, -0.042660, -0.047249, -0.049892, -0.046435, -0.028385, 0.000109, -0.337419, -4.123855, -8.168420, -13.787466, -23.032032, -32.116837, -100.313210, -32.124646, -23.045462, -13.813988, -8.209435, -4.176333, -0.358491, -0.000334, -0.027297, -0.046025, -0.050124, -0.047899, -0.043560, -0.038842, -0.013077, -0.006218, -0.002379, -0.000652, -0.000186, -0.000091, -0.000056, -0.000038, -0.000011, -0.000002};
     
-    float dB_test[sizeof(frecuencias)]; // para guardar la respuesta de nuestro filtro y compararlo con el ideal
+    float dB_test[sizeof(frecuencias)/sizeof(float)]; // para guardar la respuesta de nuestro filtro y compararlo con el ideal
     int tiempo_ciclos_filtro = 0;       // para medir el tiempo de aplicacion de cada filtro
 
     // punteros que contienen parametros del filtro
@@ -113,12 +113,13 @@ void test_unitario_filtro(void){
                 acumulador_seno = 0;
             }
             seno_int = (int)(10000.0*sin(omega*j));                   // calculo el seno (sin decimales)
+            seno = sin(omega*j);
 
             ///////////////////////////////////////////////////////////////////////////////////////////////
             /////   aplicacion y testeo del filtro con sus perifericos  ///////////////////////////////////
             // tiempo_ciclos_filtro = esp_cpu_get_cycle_count(); // tomo los ciclos actuales del procesador
             // hago e casting mediante assembler y paso de unidades la señal
-            casting_y_escala(seno_int, &seno, &k_p_m);
+            // casting_y_escala(seno_int, &seno, &k_p_m);
             // filtros en cascada
             filtro_II_d_I(&seno, x0, y0, &(punt_test+0)->b_0);  // etapa 0
             filtro_II_d_I(y0, x1, y1, &(punt_test+1)->b_0);     // etapa 1
@@ -144,7 +145,7 @@ void test_unitario_filtro(void){
     // printf("tiempo de filtrado [ns] = %d\n", tiempo_ciclos_filtro);
     printf("f [Hz]\t\tdBA_filtro\tdBA_Ideal\n");
     for(int i=0; i<(sizeof(frecuencias)/sizeof(float)); i++){
-        esp_rom_delay_us(20000);
+        vTaskDelay(pdMS_TO_TICKS(20));
         printf("%f\t%f\t%f\n", frecuencias[i], dB_test[i], dB_ideal[i]);
     }
 
@@ -156,5 +157,5 @@ void test_unitario_filtro(void){
     free(y1);
     free(y2);
     free(k_out);
-    free(punt_test);
+    // free(punt_test);
 }
